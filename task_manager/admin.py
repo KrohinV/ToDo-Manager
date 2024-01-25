25a152de114c4f81e1e84d381327d9493cacf544
KrohinV <vladimir.krohin112@gmail.com>
2024-01-25 21:21:52 +0300

    add README.md file and project structure changed

--- task_manager/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Category, Priority, Task, User
-
-admin.site.register(Category)
-admin.site.register(Priority)
-admin.site.register(Task)
-admin.site.register(User)
--- task_manager/filters.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-
-from .models import *
-
-
-class TaskFilter(django_filters.FilterSet):
-    class Meta:
-        model = Task
-        fields = ['status']
