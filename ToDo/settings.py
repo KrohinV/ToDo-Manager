946e424af8bace9946e23b5e91123dfc6a288aed
KrohinV <vladimir.krohin112@gmail.com>
2024-01-25 21:21:52 +0300

    fixed get_task bags

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'django_rest_passwordreset',
     'django_filters',
+    'djoser',
 ]
 
 # REST_FRAMEWORK = {
@@ -113,3 +114,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
--- ToDo/urls.py
+++ ToDo/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import urls
 
 from task_manager.views import *
 
 urlpatterns = [
+    path('api/v1/auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
     path('admin/', admin.site.urls),
     path('api/v1/user/', include('rest_framework.urls')),
 
